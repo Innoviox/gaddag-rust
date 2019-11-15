a44d5f88865be739d373295456cf54bbb7d69fdc
Simon Chervenak <sarras305@gmail.com>
2019-11-14 19:45:58 -0500

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let answer = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("What is your guess? ");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Higher"),
+            Ordering::Greater => println!("Lower"),
+            Ordering::Equal => {
+                println!("You win");
+                break
+            }
+        }
+    }
 }
