f88418cb22205343e8bc2084c433f06438dd9f15
Simon Chervenak <sarras305@gmail.com>
2019-11-14 19:59:10 -0500

    huh

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn main() {
-    let x = 5;
+fn thingy() -> u32 {
+    let mut counter = 0;
+    loop {
+        counter += 1;
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+        if counter == 10 {
+            break counter * 2;
+        }
+    }
+}
+
+fn main() {
+    let x = thingy();
     println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
 }
