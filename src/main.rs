601cda55c26397e700f901e4917ee0b97aeba167
Simon Chervenak <sarras305@gmail.com>
2019-11-15 08:22:08 -0500

    rectangles

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
-fn thingy() -> u32 {
-    let mut counter = 0;
-    loop {
-        counter += 1;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
 
-        if counter == 10 {
-            break counter * 2;
-        }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
-    let x = thingy();
-    println!("The value of x is: {}", x);
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
 }
