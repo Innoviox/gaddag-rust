c412eb566631c6374636c27541471723d7572860
Simon Chervenak <sarras305@gmail.com>
2019-11-24 20:37:08 -0500

    wow

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,16 @@ fn main() {
     //                 utils::Direction::Down, false);
 
 
-    board.place_move(&Move { word: "FJORD".to_string(), position: Position { row: 3, col: 7 }, direction: Down, score: 40 } );
-    board.place_move(&Move { word: "JINX".to_string(), position: Position { row: 4, col: 7 }, direction: Across, score: 36 } );
-    board.place_move(&Move { word: "ZOO".to_string(), position: Position { row: 5, col: 5 }, direction: Across, score: 32 } );
-    board.place_move(&Move { word: "WRY".to_string(), position: Position { row: 6, col: 6 }, direction: Across, score: 26 } );
-    board.place_move(&Move { word: "OHM".to_string(), position: Position { row: 3, col: 10 }, direction: Across, score: 25 } );
-    board.place_move(&Move { word: "DIG".to_string(), position: Position { row: 2, col: 8 }, direction: Across, score: 18 } );
-    board.place_move(&Move { word: "OD".to_string(), position: Position { row: 2, col: 12 }, direction: Across, score: 22 } );
-    board.place_move(&Move { word: "UTE".to_string(), position: Position { row: 1, col: 11 }, direction: Across, score: 23 } );
-    board.place_move(&Move { word: "TERTIAL".to_string(), position: Position { row: 1, col: 3 }, direction: Across, score: 68 } );
-    board.place_move(&Move { word: "EAR".to_string(), position: Position { row: 0, col: 6 }, direction: Across, score: 33 } );
+    // board.place_move(&Move { word: "FJORD".to_string(), position: Position { row: 3, col: 7 }, direction: Down, score: 40 } );
+    // board.place_move(&Move { word: "JINX".to_string(), position: Position { row: 4, col: 7 }, direction: Across, score: 36 } );
+    // board.place_move(&Move { word: "ZOO".to_string(), position: Position { row: 5, col: 5 }, direction: Across, score: 32 } );
+    // board.place_move(&Move { word: "WRY".to_string(), position: Position { row: 6, col: 6 }, direction: Across, score: 26 } );
+    // board.place_move(&Move { word: "OHM".to_string(), position: Position { row: 3, col: 10 }, direction: Across, score: 25 } );
+    // board.place_move(&Move { word: "DIG".to_string(), position: Position { row: 2, col: 8 }, direction: Across, score: 18 } );
+    // board.place_move(&Move { word: "OD".to_string(), position: Position { row: 2, col: 12 }, direction: Across, score: 22 } );
+    // board.place_move(&Move { word: "UTE".to_string(), position: Position { row: 1, col: 11 }, direction: Across, score: 23 } );
+    // board.place_move(&Move { word: "TERTIAL".to_string(), position: Position { row: 1, col: 3 }, direction: Across, score: 68 } );
+    // board.place_move(&Move { word: "EAR".to_string(), position: Position { row: 0, col: 6 }, direction: Across, score: 33 } );
     println!("{}", board);
 
     
@@ -63,8 +63,8 @@ fn main() {
 
     let mut rack = b.draw_tiles(7);
 
-    // for i in 0..20 {
-        rack = vec!['I', 'U', 'N', 'E', 'T', 'O', 'E'];
+    for i in 0..20 {
+        // rack = vec!['I', 'U', 'N', 'E', 'T', 'O', 'E'];
         println!("Rack is: {:?}", rack.clone());
         let moves = board.gen_all_moves(&rack, &t, &d, &b);
         let best_move = moves.iter().max_by(|x, y| x.score.cmp(&y.score)).unwrap();
@@ -79,7 +79,7 @@ fn main() {
         for c in b.draw_tiles(7 - rack.len()) {
             rack.push(c);
         }
-    // } 
+    } 
     // for m in board.gen_all_moves(rack, &t, &d) {
     //     println!("{} {:?}", board.place_move_cloned(&m), m);
     // }
