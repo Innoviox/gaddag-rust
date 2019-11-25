88a8978be72401984855ff6c062bac832baf93bc
Simon Chervenak <sarras305@gmail.com>
2019-11-25 08:50:40 -0500

    regression

--- leaves_analysis.py
+++ leaves_analysis.py
@@ -1,13 +1,27 @@
 from string import ascii_uppercase as au
+from random import shuffle
 alph = au + "?"
 
-x, y = [], []
+x, _y = [], []
 for i in open("resources/leaves.txt").readlines():
     a, b = i.split()
     x.append([a.count(j) for j in alph])
-    y.append(b)
+    _y.append(float(b))
+_xvar = [[j[i] for j in x] for i in range(len(alph))]
 
-xvar = [[j[i] for j in x] for i in range(len(alph))]
+n = 100000
+
+order = list(range(len(_xvar[0])))
+shuffle(order)
+
+xvar = []
+for i in _xvar:
+    xvar.append([])
+    for j in order[:n]:
+        xvar[-1].append(i[j])
+
+y = [_y[j] for j in order[:n]]
+
 print("loaded")
 
 from statsmodels.formula.api import ols
@@ -15,6 +29,7 @@ import pandas as pd
 
 X = dict(zip(alph, xvar))
 X['blank'] = X.pop('?')
+
 X['y'] = y
 
 data = pd.DataFrame(X)
