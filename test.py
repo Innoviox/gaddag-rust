67b3e937c4ed67e653709066eafbc0dcaad4f890
Simon Chervenak <sarras305@gmail.com>
2019-12-13 09:23:34 -0500

    run one game at a time, maybe some better? maybe worse? all things are unclear.

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import os
 
-cmd = "cargo run --release text -n {} -s {} -e {} > tests/test_{}.txt"
+cmd = "cargo run --release text -n {} -s {} -e {} >> tests/test_{}.txt"
 
-n = 100
+n = 1
+r = 100
 
 for i in range(10):
     # print(i)
     for j in range(10):
         if j <= i:
-            os.system(cmd.format(n, (i + 1) / 10, (j + 1) / 10, str(i+1)+str(j+1)))
+            for _ in range(r):
+                os.system(cmd.format(n, (i + 1) / 10, (j + 1) / 10, str(i+1)+str(j+1)))
         # print(f"\t{j}")
