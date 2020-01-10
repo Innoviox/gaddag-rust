7a1287bd81ff199bc6bc79ffcf8b11a949bb24e2
Simon Chervenak <sarras305@gmail.com>
2020-01-10 10:34:37 -0500

    add some nifty graphs

--- graphmaker.py
+++ graphmaker.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 p1s, p2s, ts = [], [], []
-for i, game in enumerate(open("test.txt").read().split("#character-encoding UTF-8")[1:], start=1):
+t = []
+for i, game in enumerate(open("testcogo_3000.txt").read().split("#character-encoding UTF-8")[1:], start=1):
     lines = game.split("\n")[3:]
     times = [int(i.split()[-1]) for i in lines if 'Time' in i]
     p1 = [int(i.split("+")[1].split()[0]) for i in lines if 'p1' in i]
@@ -8,15 +9,23 @@ for i, game in enumerate(open("test.txt").read().split("#character-encoding UTF-
     p1s.extend(p1)
     p2s.extend(p2)
     ts.extend(times)
-    plt.subplot(10, 10, i)
-    plt.plot(range(len(times)), times)
-    plt.axis('off')
+    for a, b in zip([times[i] for i in range(0, len(times), 2)], [i for i in lines if 'p1' in i]):
+        if False or '?' not in b: t.append(a)
+    for a, b in zip([times[i] for i in range(1, len(times), 2)], [i for i in lines if 'p2' in i]):
+        if False or '?' not in b: t.append(a)
+##    plt.subplot(10, 10, i)
+##    plt.plot(range(len(times)), times)
+##    plt.axis('off')
+##
+##    for j, bl in enumerate([i for i in lines if 'p1' in i or 'p2' in i]):
+##        n = bl.count('?')
+##        if n > 0:
+##            try:
+##                plt.plot([j], [times[j]], ' ry'[n]+'o')
+##            except: pass
 
-    for j, bl in enumerate([i for i in lines if 'p1' in i or 'p2' in i]):
-        n = bl.count('?')
-        if n > 0:
-            try:
-                plt.plot([j], [times[j]], ' ry'[n]+'o')
-            except: pass
-
+# plt.show()
+import seaborn as sns
+sns.set(style="whitegrid")
+ax = sns.violinplot(x=t)
 plt.show()
--- py/gcgplots.py
+++ py/gcgplots.py
@@ -1,6 +1,5 @@
-from statistics import mean, median
-
-time=lambda s: sum(map(int, [i.split()[-1] for i in s.split("\n") if 'Time' in i]))
+t=lambda s: map(int, [i.split()[-1] for i in s.split("\n") if 'Time' in i])
+time = lambda s: sum(t(s))
 s1s, s2s, ts = [], [], []
 with open("../testcogo_3000.txt") as file:
     s = file.read().split("#player2 p2 p2")
@@ -8,9 +7,11 @@ with open("../testcogo_3000.txt") as file:
         game = game.split("#p")[0].split("\n")
         s1 = int([i for i in game if i.startswith(">p1")][-1].split()[-1])
         s2 = int([i for i in game if i.startswith(">p2")][-1].split()[-1])
-        ts.append(time('\n'.join(game)))
+        # ts.append(time('\n'.join(game)))
+        ts.extend(t("\n".join(game)))
         s1s.append(s1)
         s2s.append(s2)
 
-print(mean(s1s), mean(s2s), mean(ts), len(s1s), median(s1s), median(s2s), median(ts))
-
+import seaborn as sns
+sns.set(style="whitegrid")
+ax = sns.violinplot(x=ts)
