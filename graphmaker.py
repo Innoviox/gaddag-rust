88c3baacafe5cb4aec3e71eb4725fd7993439d80
Simon Chervenak <sarras305@gmail.com>
2020-01-10 10:49:57 -0500

    add more

--- graphmaker.py
+++ graphmaker.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 p1s, p2s, ts = [], [], []
+g1s, g2s = [], []
 t = []
 for i, game in enumerate(open("testcogo_3000.txt").read().split("#character-encoding UTF-8")[1:], start=1):
     lines = game.split("\n")[3:]
     times = [int(i.split()[-1]) for i in lines if 'Time' in i]
     p1 = [int(i.split("+")[1].split()[0]) for i in lines if 'p1' in i]
     p2 = [int(i.split("+")[1].split()[0]) for i in lines if 'p2' in i]
+    g1s.append(sum(p1))
+    g2s.append(sum(p2))
     p1s.extend(p1)
     p2s.extend(p2)
     ts.extend(times)
@@ -27,5 +30,7 @@ for i, game in enumerate(open("testcogo_3000.txt").read().split("#character-enco
 # plt.show()
 import seaborn as sns
 sns.set(style="whitegrid")
-ax = sns.violinplot(x=t)
+# ax2 = sns.violinplot(x=g2s)
+# ax = sns.violinplot(x=g1s)
+ax = sns.violinplot(x=list(map(sum, zip(g1s, g2s))))
 plt.show()
