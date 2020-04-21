f428db24562c9e6f377adc1d01a3da9f46d4d417
Simon Chervenak <sarras305@gmail.com>
2020-04-20 22:06:45 -0400

    output works

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -6,11 +6,27 @@ pub fn main(turns: u32) {
         game.do_move();
     }
 
-    let rack: String = game.current_player().rack.iter().collect();
+    let p = game.current_player().clone();
+    let rack: String = p.rack.iter().collect();
+
+    let (moves, eval_val) = p.gen_moves(game.get_board_mut());
 
     let mut s = game.get_board().to_string();
-    s.push_str("\n");
-    s.push_str(&rack);
+    s = format!("{}{}{}{}", s, "\n", rack, "\n");
+
+    for m in moves.iter().take(50) {
+        s = format!(
+            "{}{}",
+            s,
+            format!(
+                "{} {} {} {}\n",
+                m.position.to_str(m.direction),
+                game.get_board_mut().format(&m, true),
+                m.score,
+                m.eval(1.0, eval_val)
+            )
+        );
+    }
 
     println!("{}", s);
 }
