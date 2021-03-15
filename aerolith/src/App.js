ca293f7b01f2347c2785c686c9dbf929c28c5374
Simon Chervenak <sarras305@gmail.com>
2021-03-14 22:42:59 -0400

    autofocus, some other stuff

--- aerolith/src/App.js
+++ aerolith/src/App.js
@@ -8,19 +8,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-        inputs: ['', '', '', '', '', '', '']
+        inputs: ['', '', '', '', '', '', ''],
+        words: [],
+        wordI: 0,
+        currGuess: 0
     };
 
     this.updateItem = this.updateItem.bind(this);
+    this.setWords = this.setWords.bind(this);
 
-    // fetch(process.env.PUBLIC_URL + '/files/sevens1000-out.txt')
-    //     .then(r => r.text())
-    //     .then(text => {
-    //         console.log('text decoded:', text);
-    //     });
+    fetch(process.env.PUBLIC_URL + '/files/sevens1000-out.txt')
+        .then(r => r.text())
+        .then(this.setWords);
   }
 
   input = i => document.querySelector(`input[name=input-${i}]`);
+  inputs = () => [0, 1, 2, 3, 4, 5, 6].map(this.input);
+  currWord = () => this.state.words[this.state.wordI];
+  clearInputs = () => this.inputs().map(i => i.value = "");
 
   updateItem = (i, e) => {
       this.setState({ inputs: this.state.inputs.map((item, j) => j === i ? e.target.value : item) });
@@ -29,19 +34,52 @@ class App extends Component {
 
       const next = this.input(i + 1);
       if (next === null) {
-
+          let guess = this.inputs().map(i => i.value).join("");
+          if (this.currWord().w.includes(guess)) {
+              this.setState( { currGuess: this.state.currGuess + 1}, () => this.flickerClass('green', 500, () => {
+                  this.clearInputs();
+                  if (this.state.currGuess === this.currWord().w.length) {
+                      this.setState( { wordI: this.state.wordI + 1 }, this.writeAnagram);
+                  }
+              }))
+          } else {
+              this.flickerClass('red', 500, this.clearInputs)
+          }
       } else {
           next.focus();
       }
   }
 
+  flickerClass = (cls, time, callback) => {
+      document.activeElement.blur();
+      this.inputs().map(i => {
+          i.classList.toggle(cls);
+          setTimeout(() => i.classList.toggle(cls), time);
+      });
+      setTimeout(callback, time);
+  }
+
+  setWords = (text) => {
+      this.setState({ words: text.split("\n").map(i => {
+              let [anagram, words] = i.split(",");
+              words = words.split(" ");
+              return {'a': anagram, 'w': words};
+          })}, this.writeAnagram);
+  }
+
+  writeAnagram = () => {
+      document.getElementById("test").innerHTML = this.currWord().a;
+      this.input(0).focus();
+  }
+
   render = () =>
-    <div>
+    <div id="test-div">
         <div id="inputs">
             {this.state.inputs.map((item, i) => (
                 <input maxLength={1} key={i} name={`input-${i}`} className="test-input" type="text" onChange={e => this.updateItem(i, e)} />
             ))}
         </div>
+        <span id="test" />
     </div>;
 }
 
