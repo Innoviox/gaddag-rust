777f0893603b380229538e3399463fa627349f48
Simon Chervenak <sarras305@gmail.com>
2021-04-03 23:53:02 -0400

    shuffle thingy

--- aerolith/src/App.js
+++ aerolith/src/App.js
@@ -61,8 +61,27 @@ class App extends Component {
       setTimeout(callback, time);
   }
 
+  shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
   setWords = (text) => {
-      this.setState({ words: text.split("\n").map(i => {
+      this.setState({ words: this.shuffle(text.split("\n")).map(i => {
               let [anagram, words] = i.split(",");
               words = words.split(" ");
               return {'a': anagram, 'w': words};
