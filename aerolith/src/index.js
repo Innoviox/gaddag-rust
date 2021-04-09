42f9ca2784899a2b99ab881d00323dd7c8f0e416
Simon Chervenak <sarras305@gmail.com>
2021-04-08 21:36:20 -0400

    cool animation time
    also commit pumping time :D

--- aerolith/src/index.js
+++ aerolith/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
